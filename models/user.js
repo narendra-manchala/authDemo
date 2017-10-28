1cd05960cf9ebd18402317da29da453d4b27d581
narendra67 <mvnarendrareddy@gmail.com>
2017-10-29 00:28:55 +0530

    refactoring

--- models/user.js
+++ models/user.js
@@ -1,15 +1,3 @@
-// var mongoose = require("mongoose");
-// var passportLocalMongoose = require("passport-local-mongoose");
-//
-// var userSchema =new mongoose.Schema({
-//     username: String,
-//     password: String
-// })
-//
-// userSchema.plugin(passportLocalMongoose);
-//
-// var User = mongoose.model("User", userSchema);
-
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
@@ -20,7 +8,7 @@ var userSchema = new mongoose.Schema({
 
 userSchema.plugin(passportLocalMongoose);
 
-var User = mongoose.model("User", userSchema);
+// var User = mongoose.model("User", userSchema);
 
 
 module.exports = mongoose.model("User", userSchema);
