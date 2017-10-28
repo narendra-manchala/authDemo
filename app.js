5eb8e1605963e9cc2e286b592661c10d70ab2f57
narendra67 <mvnarendrareddy@gmail.com>
2017-10-28 19:58:41 +0530

    2nd commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var passport = require("passport")
 var LocalStrategy = require("passport-local")
 var passportLocalMongoose = require("passport-local-mongoose")
 var bodyParser = require("body-parser");
-var User = require("./models/user");
+const User = require("./models/user");
 var expressSession = require("express-session");
 
 mongoose.connect("mongodb://localhost/authdemo");
@@ -21,11 +21,20 @@ app.use(expressSession({
 }))
 app.use(passport.initialize());
 app.use(passport.session());
+// passport.use(User.createStrategy());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser())
+
+// passport.use(new LocalStrategy(User.authenticate()));
 
 //reading a session tasking data from session thats encoded and unencoding it (deserialize does this) and
 // encoding it and puting it back in the session (serialize does this)
 // this serialise and deserialize are added automatically by passport mongoose used in user.js file
 // passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+// const User = require('./models/user');
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
